021bfa9e774b0c69d1dcd8dee4a7bf1661a26a13
Arctusgt <xenaxial@gmail.com>
2026-07-12 11:07:46 +0800

    yank work?

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -4,4 +4,17 @@
 
 vim.opt.guicursor = ""
 vim.opt.nu = true
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
 vim.opt.clipboard = "unnamedplus"
